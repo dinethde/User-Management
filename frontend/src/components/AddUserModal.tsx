55e24a2b98f202b36a24e8a46f29097055592f1d
Dineth <your-email@example.com>
2025-06-25 00:01:58 +0530

    addUserModal component.

--- frontend/src/components/AddUserModal.tsx
+++ frontend/src/components/AddUserModal.tsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
+
 function AddUserModal() {
+  const [formData, setFormData] = useState({
+    name: "",
+    age: "",
+    emial: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Form data:", formData);
+    // Add your submit logic here
+  };
   return (
-    <div>
-      <div></div>
+    <div className="w-5/12 border border-[#CCCCCC] rounded-lg shadow-md p-4">
+      <form action="">
+        {/* Header */}
+        <div className="flex flex-row justify-between items-center">
+          <h3 className="text-3xl">Add n new user</h3>
+          <button>
+            <img src="/x_vector.svg" className="w-4 h-4" />
+          </button>
+        </div>
+
+        {/* Form inputs*/}
+        <div>
+          <p>Label</p>
+        </div>
+      </form>
     </div>
   );
 }
--- frontend/src/pages/UserManagement.tsx
+++ frontend/src/pages/UserManagement.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { getAllUsers } from "../services/user-api";
 import UserTable from "../components/UserTable";
 import TableHeader from "../components/TableHeader";
+import AddUserModal from "../components/AddUserModal";
 
 function UserManagement() {
   return (
@@ -9,6 +10,7 @@ function UserManagement() {
       <div className="w-2/3">
         <TableHeader />
         <UserTable />
+        <AddUserModal />
       </div>
     </div>
   );
